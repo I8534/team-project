81c042ef7e6b63ef873f98895e8d7e473a9d1fbc
I8534 <I85341266@GMAIL.COM>
2023-04-11 15:39:39 +0900

    어처구니없는 이유

--- combine/src/Main/Mainpage.jsx
+++ combine/src/Main/Mainpage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MainBanner from './MainBanner/MainBanner'
 import Box1 from './Box1/Box1'
 import Box2 from './Box2/Box2'
-import Box3 from './Box3/Box3'
+// import Box3 from './Box3/Box3'
 import Gesifan from './Box4/Gesifan'
 import Cctv from './Box5/Cctv'
 import MainFooter from './Footer/MainFooter'
@@ -16,7 +16,7 @@ const Mainpage = () => {
       <MainBanner/>
       <Box1/>
       <Box2/>
-      <Box3/>
+      {/* <Box3/> */}
       <Gesifan/>
       <Cctv/>
       <MainFooter/>
